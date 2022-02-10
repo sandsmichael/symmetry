5a229822cdbfcb8c6b4195d10d99ffda76e2bd0b
Michael Sands <michaelesands@gmail.com>
2022-02-10 00:25:35 -0500

    pare

--- usagetest.py
+++
@@ -1,7 +0,0 @@
-from comparison import Comparison
-import pandas as pd
-
-dfx = pd.DataFrame({'1': [1, 2, 3], '2': [4, 5, 6]})
-dfy = pd.DataFrame({'1': [4, 5, 6], '2': [3, 2, 1]})
-
-Comparison(dfx, dfy)
